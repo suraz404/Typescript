562771c2cdde00859cb9329cdfd71c9e8a1ddb5b
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-07-01 19:57:12 +0545

    Refactor hello.ts to comment out example code and improve function clarity

--- hello.ts
+++ hello.ts
@@ -1,7 +1,37 @@
-type Person = {
-  name: string;
+// type Person = {
+//   name: string;
+// };
+// const person: Person = {
+//   name: "John",
+// };
+// console.log(person.name);
+
+//string
+// let myName: string = "Suraz";
+// console.log(myName);
+//number
+// let myRollNo: number = 21;
+// console.log(myRollNo);
+//boolean
+// let myBool: boolean = true;
+// console.log(myBool);
+
+//any datatype
+// let favVar: any = 90;
+// favVar = "Surax";
+// console.log(favVar);
+//no error
+
+function add(x: number, y: number): number {
+  return x + x;
+}
+
+console.log(typeof add(9, 4));
+
+//leaaarning about void
+
+const display = (x: string): void => {
+  console.log(`hello ${x}`);
 };
-const person: Person = {
-  name: "John",
-};
-console.log(person.name);
+
+display("Suraz");
